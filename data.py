53fb81b5394d4fc9e95ea21e9af60ec80b01d8e1
Nicholas Brown <nwbrown@gmail.com>
2025-04-07 16:45:01 -0400

    Add harmonics processor

--- data.py
+++ data.py
@@ -8,6 +8,8 @@ from torch.utils.data import Dataset
 import torch.nn.functional as F
 import librosa
 from mimir.training import DEVICE
+from multiprocessing import Queue, Process
+from queue import Empty
 
 def get_fnames(folder):
     return sorted(glob.glob(f"{folder}/*.ogg"))
@@ -29,8 +31,7 @@ class BirdClefTrainAudio():
 
     def __getitem__(self, idx):
         fname, label = self.data[idx]
-        y, sr = librosa.load(fname, duration=self.max_duration, sr=self.sr)
-        return y, label
+        return fname, label
 
     def label_weights(self):
         freqs = Counter([label for _, label in self.data])
@@ -45,6 +46,53 @@ class BirdClefSTFTData(Dataset):
         return len(self.audio)
 
     def __getitem__(self, idx):
-        y, label = self.audio[idx]
+        fname, label = self.audio[idx]
+        y, sr = librosa.load(fname, duration=self.max_duration, sr=self.sr)
         stft = np.abs(librosa.stft(y))
         return torch.tensor(stft.T, dtype=torch.float), torch.tensor(label, dtype=torch.long)
+
+def process_harmonics(sr: int, max_duration: int, max_harmonic: int, fmin: int, fmax: int, in_queue: Queue, out_queue: Queue):
+    hs = np.arange(1, max_harmonic)
+    freqs = librosa.fft_frequencies(sr=sr)
+    while True:
+        try:
+            next = in_queue.get(block=True, timeout=5)
+        except Empty:
+            return
+        fname, label = next
+        y, sr = librosa.load(fname, duration=max_duration, sr=sr)
+        f0, v, vp = librosa.pyin(y=y, sr=sr, fmin=fmin, fmax=fmax, fill_na=0)
+        s = np.abs(librosa.stft(y))
+        harmonic_energy = librosa.f0_harmonics(s, f0=f0, harmonics=hs, freqs=freqs)
+        out_queue.put((f0, harmonic_energy, label))
+
+class BirdClefHarmonics(Dataset):
+    """Dataset for the harmonics of the audio data"""
+    def __init__(self, audio: BirdClefTrainAudio, fmin=50, fmax=4000, max_harmonic=11, num_processes=10):
+        self.n = len(audio)
+        in_queue=Queue(self.n)
+        out_queue=Queue()
+        for _ in range(num_processes):
+            p = Process(target=process_harmonics, args=[audio.sr, audio.max_duration, max_harmonic, fmin, fmax, in_queue, out_queue])
+            p.start()
+        self.f0s = []
+        self.harmonics = []
+        self.labels = []
+        for fname, label in audio:
+            in_queue.put((fname, label))
+        for i in range(self.n):
+            f0, harmonic_energy, label = out_queue.get()
+            self.f0s.append(f0)
+            self.harmonics.append(harmonic_energy)
+            self.labels.append(label)
+            print(f"[{i:>5d}/{self.n:>5d}]", end="\r")
+
+    def __len__(self):
+        return self.n
+
+    def __getitem__(self, idx):
+        f0 = self.f0s[idx]
+        harmonics = self.harmonics[idx]
+        input = np.concatenate([f0.reshape((1,-1)), harmonics], axis=0)
+        label = self.labels[idx]
+        return input, label
