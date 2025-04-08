18270dc7861e591ce76cf00587f650ef0ea09ebd
Nicholas Brown <nwbrown@gmail.com>
2025-04-08 07:58:55 -0400

    Add harmonic model

--- model.py
+++ model.py
@@ -1,6 +1,8 @@
+import math
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.nn.init import xavier_uniform_
 
 class ConvModel(nn.Module):
     """A convultional model"""
@@ -28,4 +30,23 @@ class ConvModel(nn.Module):
         y = F.relu(self.fc1(y))
         # y is of the shape (N, 512)
         y = self.fc2(y)
+        return y
+
+class HarmonicModel(nn.Module):
+    """A model using the root note and harmonics"""
+    def __init__(self, n_labels, n_harmonics, kernel_size=10, num_filter_maps=16, dropout=0.5):
+        super(HarmonicModel, self).__init__()
+        self.conv = nn.Conv1d(n_harmonics+1, num_filter_maps, kernel_size,
+                              padding=int(math.floor(kernel_size/2)))
+        xavier_uniform_(self.conv.weight)
+        self.u = nn.Linear(num_filter_maps, 1)
+        xavier_uniform_(self.u.weight)
+        self.final = nn.Linear(num_filter_maps, n_labels)
+        xavier_uniform_(self.final.weight)
+
+    def forward(self, input):
+        conved = torch.tanh(self.conv(input.transpose(1,2)))
+        attention = F.softmax(torch.matmul(self.u.weight, conved), dim=2)
+        v = torch.matmul(attention, conved.transpose(1,-1))
+        y = torch.sigmoid(self.final(v)).squeeze()
         return y
